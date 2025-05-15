27431b2b859d6a12c1f7a8b78e466bac346fa556
isnteres <diegogaliano2005@gmail.com>
2025-05-15 09:50:07 -0500

    rectificacion de codigo y mejoramiento para que las secciones se puedan seleccionar

--- php1.0/productos/panes.php
+++ php1.0/productos/panes.php
@@ -5,64 +5,64 @@
         <div class="product-container">
     
             <div class="product">
-                <img src="../imagenes/Aceituna.jpg" alt="Pan Serranito" width="150">
+                <img src="../imagenes/serrano.jpg" alt="Pan Serranito" width="150">
                 <p>Pan serranito</p>
-                <p class="price">S/ 2.50</p>
+                <p class="price">S/ 1.00 x 4u</p>
                 <button class="button-1">Agregar</button>
             </div>
     
             <div class="product">
                 <img src="../imagenes/queso.jpg" alt="Pan con Queso" width="150">
-                <p>Pan con Aceituna</p>
+                <p>Pan con Queso</p>
                 <p class="price">S/ 2.50</p>
                 <button class="button-1">Agregar</button>
             </div>
             <div class="product">
                 <img src="../imagenes/quesoyoregano.png" alt="Pan con Queso y Oregano" width="150">
-                <p>Pan con Aceituna</p>
+                <p>Pan con Queso y Oregano</p>
                 <p class="price">S/ 2.50</p>
                 <button class="button-1">Agregar</button>
             </div>
             <div class="product">
                 <img src="../imagenes/pollohorno.jpg" alt="Pan con Pollo al Horno" width="150">
-                <p>Pan con Aceituna</p>
+                <p>Pan con Pollo al Horno</p>
                 <p class="price">S/ 2.50</p>
                 <button class="button-1">Agregar</button>
             </div>
             <div class="product">
-                <img src="../imagenes/cabanossiyqueso" alt="Pan con Cabanossi y Queso" width="150">
-                <p>Pan con Aceituna</p>
+                <img src="../imagenes/cabanossiyqueso.png" alt="Pan con Cabanossi y Queso" width="150">
+                <p>Pan con Cabanossi y Queso</p>
                 <p class="price">S/ 2.50</p>
                 <button class="button-1">Agregar</button>
             </div>
             <div class="product">
                 <img src="../imagenes/jamon.jpg" alt="Pan con Cabanossi y Jamon" width="150">
-                <p>Pan con Aceituna</p>
+                <p>Pan con Cabanossi y Jamon</p>
                 <p class="price">S/ 2.50</p>
                 <button class="button-1">Agregar</button>
             </div>
             <div class="product">
-                <img src="../imagenes/chocolate" alt="Pan con Chocolate y Pecanas" width="150">
-                <p>Pan con Aceituna</p>
+                <img src="../imagenes/chocolate.jpg" alt="Pan con Chocolate y Pecanas" width="150">
+                <p>Pan con Chocolate con Pecana</p>
                 <p class="price">S/ 2.50</p>
                 <button class="button-1">Agregar</button>
             </div>
             <div class="product">
                 <img src="../imagenes/manjar.jpg" alt="Pan con Manjar" width="150">
-                <p>Pan con Aceituna</p>
+                <p>Pan con Manjar</p>
                 <p class="price">S/ 2.50</p>
                 <button class="button-1">Agregar</button>
             </div>
             <div class="product">
                 <img src="../imagenes/huachana.jpg" alt="Pan con Salchicha Huachana" width="150">
-                <p>Pan con Aceituna</p>
+                <p>Pan con Salchicha Huachana</p>
                 <p class="price">S/ 2.50</p>
                 <button class="button-1">Agregar</button>
             </div>
             <div class="product">
                 <img src="../imagenes/pizza.jpg" alt="Mini Pizza Americanas" width="150">
-                <p>Pan con Aceituna</p>
-                <p class="price">S/ 2.50</p>
+                <p>Mini pizza Americana</p>
+                <p class="price">S/ 5.00</p>
                 <button class="button-1">Agregar</button>
             </div>
 
