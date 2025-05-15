51badd7eda73eec4432d958653e2a7203a9093c6
isnteres <diegogaliano2005@gmail.com>
2025-05-14 21:10:33 -0500

    mas .. para que funcione las cosas

--- php1.0/productos/bebidas.php
+++ php1.0/productos/bebidas.php
@@ -1,32 +1,67 @@
-<?php require('./layout/header1.php') ?>
+<?php require('../layout/header1.php') ?>
 
-<?php require('./layout/heaproductos.php') ?>
+<?php require('../layout/heaproductos.php') ?>
+
 
 <main>
     <div class="product-container">
         <!-- Aquí irían los productos de bebidas -->
         <div class="product">
-            <img src="imagenes/cafe.jpg" alt="Café Americano" width="150">
+            <img src="../imagenes/americano.jpg" alt="Café Americano" width="150">
             <p>Café Americano</p>
             <p class="price">S/ 3.00</p>
             <button class="button-1">Agregar</button>
         </div>
 
         <div class="product">
-            <img src="imagenes/te.jpg" alt="Té Verde" width="150">
+            <img src="../imagenes/chocolatecaliente.jpg" alt="Chocolate Caliente" width="150">
+            <p>Chocolate Caliente</p>
+            <p class="price">S/ 3.50</p>
+            <button class="button-1">Agregar</button>
+        </div>
+
+        <div class="product">
+            <img src="../imagenes/Café_canela.jpg" alt="Cafe_Canela" width="150">
+            <p>Cafe con Canela</p>
+            <p class="price">S/ 3.50</p>
+            <button class="button-1">Agregar</button>
+        </div>
+
+        <div class="product">
+            <img src="../imagenes/cafe_vainilla.jpg" alt="cafe_vainilla" width="150">
+            <p>Cafe con vainilla</p>
+            <p class="price">S/ 3.50</p>
+            <button class="button-1">Agregar</button>
+        </div>
+
+        <div class="product">
+            <img src="../imagenes/te_verde.jpg" alt="Té Verde" width="150">
             <p>Té Verde</p>
             <p class="price">S/ 2.50</p>
             <button class="button-1">Agregar</button>
         </div>
 
         <div class="product">
-            <img src="imagenes/chocolatecaliente.jpg" alt="Chocolate Caliente" width="150">
-            <p>Chocolate Caliente</p>
+            <img src="../imagenes/Manzanilla.jpg" alt="Manzanilla" width="150">
+            <p>Manzanilla</p>
+            <p class="price">S/ 1.50</p>
+            <button class="button-1">Agregar</button>
+        </div>
+
+        <div class="product">
+            <img src="../imagenes/Hierba Luisa.png" alt="Hierba Luisa" width="150">
+            <p>Hierba Luisa</p>
+            <p class="price">S/ 2.00</p>
+            <button class="button-1">Agregar</button>
+        </div>
+
+        <div class="product">
+            <img src="../imagenes/Emoliente caliente.jpg" alt="Emoliente_Caliente" width="150">
+            <p>Emoliente</p>
             <p class="price">S/ 3.50</p>
             <button class="button-1">Agregar</button>
         </div>
 
-        <!-- Puedes agregar más productos de bebidas de manera similar -->
     </div>
 </main>
 
