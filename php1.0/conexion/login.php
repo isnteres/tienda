139aa3d74e5ff0944a82d487b4ef76bf6fc4383d
isnteres <diegogaliano2005@gmail.com>
2025-05-21 21:50:05 -0500

    cambios de inicio

--- php1.0/conexion/login.php
+++ php1.0/conexion/login.php
@@ -6,19 +6,16 @@ $username = "root";
 $password = "";
 $database = "fuegoancestral";
 
-
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = trim($_POST['email']);
     $contraseña = trim($_POST['contraseña']);
 
-    
     $sql = "SELECT * FROM usuarios WHERE email = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $email);
@@ -29,17 +26,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $usuario = $result->fetch_assoc();
 
         if (password_verify($contraseña, $usuario['contraseña'])) {
-            $_SESSION['usuario'] = $usuario['email']; 
-            
-
+            $_SESSION['usuario'] = $usuario['email'];
             header("Location: ../perfil.php");
             exit();
+
         } else {
-            
             echo "<script>alert('Contraseña incorrecta'); window.location.href='../inicio.php';</script>";
         }
     } else {
-        
         echo "<script>alert('El usuario no existe'); window.location.href='../inicio.php';</script>";
     }
 
